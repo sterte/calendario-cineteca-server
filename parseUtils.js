900689eccbe18835aa6d37a3861628b8b8ef4450
sterte@gmail.com <sterte@gmail.com>
2021-09-14 20:20:13 +0200

    Parse of ... chrachter added

--- parseUtils.js
+++ parseUtils.js
@@ -1,5 +1,7 @@
 exports.forceCharachtersEncoding = (text) => {
     text = text.replace(/&#8217;/g, '\'');
-    text = text.replace(/&#8211;/g, '-');    
+    text = text.replace(/&#8211;/g, '-');   
+    text = text.replace(/&#8230;/g, '...');   
+    
     return text;
 };
