b78b20d6f1e50bc1a5de7732639591cebbd6003f
sterte@gmail.com <sterte@gmail.com>
2021-10-13 18:03:56 +0200

    special charachter (quote) parse added

--- functions/parseUtils.js
+++ functions/parseUtils.js
@@ -2,7 +2,7 @@ exports.forceCharachtersEncoding = (text) => {
     text = text.replace(/&#8217;/g, '\'');
     text = text.replace(/&#8211;/g, '-');   
     text = text.replace(/&#8230;/g, '...');   
-    
+    text = text.replace(/&#8216;/g, "'");      
     return text;
 };
 
